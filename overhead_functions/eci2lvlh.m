19d516f107ac79aceaa000f06b8b764f1411edac
Michael Johnston <MichaelTJohnston@Gmail.com>
2018-05-17 16:41:49 -0700

    Updated transformation matrix
    ECI to LVLH transformation function and main script.

--- overhead_functions/eci2lvlh.m
+++ overhead_functions/eci2lvlh.m
@@ -2,19 +2,13 @@ function [C] = eci2lvlh(r,v)
     % returns the transformation matrix from lvlh to eci
     
     % defining lvlh basis
-    z2 = -r/norm(r);
-    y2 = -(cross(r,v))/norm((cross(r,v)));
-    x2 = cross(y2,z2);
+    z1 = -r/norm(r);
+    y1 = -(cross(r,v))/norm((cross(r,v)));
+    x1 = cross(y1,z1);
+	F_lvlh = [x1'; y1'; z1';];
     
     % defining eci basis
-    x1 = [1;0;0];
-    y1 = [0;1;0];
-    z1 = [0;0;1];
+	F_eci = diag([1,1,1]);
     
-    C = [dot(x2,x1) dot(x2,y1) dot(x2,z1);...
-        dot(y2,x1) dot(y2,y1) dot(y2,z1);...
-        dot(z2,x1) dot(z2,y1) dot(z2,z1)];
-    
-    
+    C = F_lvlh*F_eci';
 end
-
