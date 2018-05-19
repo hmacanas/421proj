d2b4a8491da35b4e0a925855b378f43400832972
Michael Johnston <MichaelTJohnston@Gmail.com>
2018-05-18 19:44:00 -0700

    Added magnetic field torque.

--- proj.m
+++ proj.m
@@ -127,8 +127,10 @@ ns = [1;0;0]; % Constant in ECI
 I = consts.I; % Spacecraft inertia matrix
 muearth = 398600;
 
+R = state(11:13);
+
 % nagnitude of r vector
-r_mag = norm(state(11:13));
+r_mag = norm(R);
 
 % Transformation matrix from ECI to body
 C_b_ECI = cx(state(1))*cy(state(2))*cz(state(3));
@@ -138,7 +140,7 @@ ns_b = C_b_ECI*ns;
 COM = consts.COM; % SC center of mass
 
 % Velocity vector in body
-v_b =  C_b_ECI*state(11:13);
+v_b =  C_b_ECI*R;
 
 
 % if no torque set torques to zero
@@ -146,22 +148,25 @@ if strcmp(Torque,'no')
     T = [0;0;0];
 else
     % gravity torque
-    rb = C_b_ECI*state(11:13);
+    rb = C_b_ECI*R;
     T_g = 3*muearth/r_mag^5*cross_matrix(rb)*I*rb;
 
     % srp torque
 	 T_srp = srp(consts.n,consts.rho,ns_b,consts.A);
-    % magnetic torque
-	
+	 
+    % -- Magnetic torque calculation --
+	dateTimeVec = datevec(datetime([2018 3 20 12 09 01])+seconds(t(end))); % Date & time
+	latLong = eci2lla(R'.*1E3,dateTimeVec); % Lat/long for world mag [deg?]
+	magVec_eci = wrldmagm((norm(R)-6378)*1E3, latLong(1), latLong(2), decyear(dateTimeVec))*1E-9; % Magnetic field vector [T]
+	resMag_b = [0;0;-0.5]; % Spacecraft residual magnetic torque [A*m^2]
+	T_mag = cross(C_b_ECI*magVec_eci,resMag_b) % Magnetic torque calc
 	
     % atmospheric drag torque
 	 T_drag = drag(consts.n,consts.rho,v_b,consts.A);
 	
     % total torque
-    T = T_g + T_srp + T_drag;
+    T = T_g + T_srp + T_drag + T_mag;
 end
-
-
     
 % attitude motion equatiuons eci
 wdot_eci = I\(T - cross(state(4:6),I*state(4:6)));
@@ -174,7 +179,7 @@ eulrates_lvlh = euler_rates(state(20:22),state(17),state(18));
 quaternion_rates_lvlh = quatrates(state(20:22),state(23:26));
 
 % orbital motion equations
-acc = -muearth*state(11:13)./r_mag^3;
+acc = -muearth*R./r_mag^3;
 
 % outputs that will be intergrated 
 y = [eulrates_eci;wdot_eci;quaternion_rates_eci;state(14:16); acc;eulrates_lvlh;wdot_lvlh;quaternion_rates_lvlh];
